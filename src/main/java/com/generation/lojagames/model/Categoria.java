a657a5fc06bfcbcd6b90b547566ed6f9387a2f15
Antonio Bandeira <antonioviniciusvb@outlook.com>
2023-12-14 17:06:33 -0300

    Criação da Model, Controller e Repository da Categoria

---
+++ src/main/java/com/generation/lojagames/model/Categoria.java
@@ -0,0 +1,40 @@
+package com.generation.lojagames.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo [tipo] é obrigatório!")
+	@Size(min = 3, max = 255, message = "O [tipo] deve conter no mínimo 10 e no máximo 255 caracteres.")
+	@Column(length = 255, nullable = false)
+	private String tipo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
